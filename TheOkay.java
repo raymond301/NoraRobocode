50ca62541558d9d8a2ae978dc9341adffb56ec21
Nora Dixon <noradixon03@gmail.com>
2016-06-27 18:58:16 -0500

    :)
    :)

--- TheOkay.java
+++ TheOkay.java
@@ -14,37 +14,51 @@ public class TheOkay extends Robot
 	 */
 	public void run() {
 		// Initialization of the robot should be put here 
-		(0,0)
-
+		
 		// After trying out your robot, try uncommenting the import at the top,
 		// and the next line:
 
 		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
 
 		// Robot main loop
+		int c =1;
 		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+ 			System.out.println(c+"  X="+getX());
+			System.out.println(c+"  Y="+getY());
+			
+			
+// Replace the next 4 lines with any behavior you would like
+		turnGunRight(50);
+			//back(1);			
+		
+			//back(100);
+			//turnGunRight(360);
+		//	onScannedRobotEvent ();
+		c++;
 		}
 	}
 
 	/**
 	 * onScannedRobot: What to do when you see another robot
 	 */
-	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		fire(1);
-	}
+	public void  onScannedRobot(ScannedRobotEvent e) {
+       //fire(1);
+// Assuming radar and gun are aligned...
+       if (e.getDistance() >200) {
+           fire(4);
+       } else {
+           fire(2);
+		   
+       }
+   } 
 
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
 		// Replace the next line with any behavior you would like
-		back(10);
+		//back(5);
+	//	ahead(3);
 	}
 	
 	/**
@@ -54,4 +68,13 @@ public class TheOkay extends Robot
 		// Replace the next line with any behavior you would like
 		back(20);
 	}	
-}
+
+/*void onScannedRobot(ScannedRobotEvent event) {
+       // Assuming radar and gun are aligned...
+       if (event.getDistance() < 25) {
+           fire(3);
+       } else {
+           fire(1);
+       } 
+	   */
+   } 
